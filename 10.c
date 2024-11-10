f1e9505f9c43f37420440176520c2b2f88aa9538
zgJQ08 <121170329+zgJQ08@users.noreply.github.com>
2024-11-10 22:40:31 +0800

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    
+    setlocale(LC_ALL, ""); //讓中文能正常顯示
+
+    int N;
+    scanf("%d", &N);
+    
+    char A[] = "甲乙丙丁戊己庚辛壬癸";
+    char B[] = "子丑寅卯辰巳午未申酉戌亥";
+    
+    char *pA = A + (N % 10) * 3; //用指標
+    char *pB = B + (N % 12) * 3;
+
+    printf("%.3s%.3s\n", pA, pB); //中文占3字節
+
+}
