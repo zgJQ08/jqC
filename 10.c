c8b583fbeccddbde436be130ec1dfa9106e28ae1
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-10 22:51:38 +0800

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-#include <locale.h> //
+#include <locale.h> // loca"l"e
 
-int main() {
-    
-    setlocale(LC_ALL, ""); //讓中文能正常顯示
+int main(){
+  	setlocale(LC_ALL,""); //
+	int N;
+  	scanf("%d",&N);
+  	char A[]="甲乙丙丁戊己庚辛壬癸";
+	char B[]="子丑寅卯辰巳午未申酉戌亥";
+	char *pa = A+ (N%10)*3 ; //用指標 //中文占三個字節
+    char *pb = B + (N%12)*3 ;
+    printf("%.3s%.3s",pa,pb); // 
 
-    int N;
-    scanf("%d", &N);
-    
-    char A[] = "甲乙丙丁戊己庚辛壬癸";
-    char B[] = "子丑寅卯辰巳午未申酉戌亥";
-    
-    char *pA = A + (N % 10) * 3; //用指標
-    char *pB = B + (N % 12) * 3;
-
-    printf("%.3s%.3s\n", pA, pB); //中文占3字節
 
 }
