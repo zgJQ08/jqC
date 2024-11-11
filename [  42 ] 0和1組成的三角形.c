01e765eec7f0c56912ce109aad3be9c945bb2524
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-11 19:27:32 +0800

    Create [  42  ] 0和1組成的三角形.c

---
+++ [  42 ] 0和1組成的三角形.c
@@ -0,0 +1,22 @@
+/* for用1~N方便判斷，基偶數分開處理 */
+#include <stdio.h>
+	int main(){
+    	int N;
+      	scanf("%d",&N);
+      	for(int i=1 ; i<=N ; i++){
+        	if(i%2==1){
+            	for(int j=1 ; j<=i ; j++){
+            	    if(j%2==1) printf("1");
+            	    else printf("0");
+            	}
+            }else{
+                for(int j=1 ; j<=i ; j++){
+            	    if(j%2==1) printf("0");
+            	    else printf("1");
+            	}
+            }
+            printf("\n");
+        }
+    
+    
+    }
