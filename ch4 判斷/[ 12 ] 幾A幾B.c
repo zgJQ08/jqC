3b2a7a7e124ff3e1b95b99e74adda29206fee468
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-11 23:59:09 +0800

    Update and rename 幾A幾B to [ 12 ] 幾A幾B.c

--- ch4 判斷/[ 12 ] 幾A幾B.c
+++ ch4 判斷/[ 12 ] 幾A幾B.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
     int main(){
         int a,b ;
-        scanf("%d%d",a,b);
+        scanf("%d%d",&a,&b);
         int A[4] , B[4] ;
         for(int i=0 ; i<4 ; i++){
             A[i] = a%10 ; B[i] = b%10 ;
@@ -10,11 +10,13 @@
         int nA =0 , nB=0 ;
         
         for(int i=0 ; i<4 ; i++){
-            for(int i=0 ; i<4 ; i++){
-            A[i] = a%10 ; B[i] = b%10 ;
-            a /= 10 ; b/=10 ;
+            for(int j=0 ; j<4 ; j++){
+            if(A[i] == B[j] ){
+                if(i==j) nA ++;
+                else nB++;
+            }
         }
-
         }
+        printf("%dA%dB",nA,nB);
         
     }
