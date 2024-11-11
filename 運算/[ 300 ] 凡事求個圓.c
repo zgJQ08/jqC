e069b62c6024d9af0e9c3a886952cf2b130f2612
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-11 19:49:12 +0800

    Update [ 300 ] 凡事求個圓.c

--- 運算/[ 300 ] 凡事求個圓.c
+++ 運算/[ 300 ] 凡事求個圓.c
@@ -1,12 +1,15 @@
-/* 圓周率要背，除非有其他解法^^ */
-#define PI 3.141592653589793
+/* #順序要對 ，然後PI是M_PI */
+#define _GNU_SOURCE
+#include <math.h> 
 #include <stdio.h>
-	int main(){
-    	float R;
-      	scanf("%f",&R);
-      	printf("pi=%.5f\n",PI);
-      	printf("radius=%.0f\n",R);
-      	printf("circumference=%.5f\n",R*2*PI);
-        printf("area=%.5f\n",R*R*PI);
-    
-    }
+
+int main(){
+    float R;
+    scanf("%f", &R);
+    printf("pi=%.5f\n", M_PI); 
+    printf("radius=%.0f\n", R);
+    printf("circumference=%.5f\n", R * 2 * M_PI);
+    printf("area=%.5f\n", R * R * M_PI);
+
+    return 0;
+}
