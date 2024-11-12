ced2cf07f9566dafce4427560bc42d74d5dac419
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-12 15:12:46 +0800

    Update and rename [ 173 ] 基本輸入練習 to [ 173 ] 基本輸入練習.c

---
+++ ch2 基本程式架構 /[ 173 ] 基本輸入練習.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+	int main(){
+    	int age ;
+      	float height ;
+      	scanf("%d%f",&age,&height);
+      	printf("Your age is: %d\n",age);
+      	printf("Your height is: %.5f\n",height);
+
+    
+	}
