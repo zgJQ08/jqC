aeaedf3790371280ed8504e46e0c9143e42e14e7
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-12 18:33:15 +0800

    Create [ 21 ] Power of Cryptography.c

---
+++ ch4 判斷/[ 21 ] Power of Cryptography.c
@@ -0,0 +1,14 @@
+/* 1.這題用double ，exp(log(p)/n) */
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double n, p;
+    scanf("%lf %lf", &n, &p);
+    
+    double t = exp(log(p)/n);
+    printf("%0.lf\n", t);
+
+    
+    return 0;
+}
