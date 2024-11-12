d94a4b2ffbd1a940978b9a1b06c63628262cd4c6
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-12 23:11:22 +0800

    Update [ CPE ] Open Credit System.c

--- CPE/[ CPE* ] Open Credit System.c
+++ CPE/[ CPE* ] Open Credit System.c
@@ -1,3 +1,4 @@
+/* 要從最菜的算到最資深的 然後有點難 */
 #include <stdio.h>
 
 long long Num[100000];
@@ -13,10 +14,9 @@ int main() {
             scanf("%lld", &Num[i]);
         }
       
-//從最菜的計算到最資深的
       
         long long MxD = Num[0] - Num[1];  
-        long long min_junior_score = Num[Ns - 1];  //最菜的分數
+        long long min_junior_score = Num[Ns - 1]; 
         for (int i = Ns - 2; i >= 0; i--) { 
             
             long long D = Num[i] - min_junior_score;
