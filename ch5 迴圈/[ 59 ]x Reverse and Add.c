986ac6c6cd9e5681af78aec7e0c5e5e0151fc8bf
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-12 19:21:56 +0800

    Update [ 59 ]x Reverse and Add.c

--- ch5 迴圈/[ 59 ]x Reverse and Add.c
+++ ch5 迴圈/[ 59 ]x Reverse and Add.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <limits.h> // 需要用到 INT_MAX
 
+int main(){
+    int times;
+    scanf("%d", &times);
+    while(times--){
+        int Num, l = 0 ,F=0 , t=0;
+        scanf("%d", &Num);
+        int Mun;
+        
+        do{
+            t++;   
+            Mun = 0; // 記得重置
+            int temp = Num;
+            l = 0;
+            int M[100];
+            
+            while(temp > 0){
+                Mun = Mun * 10 + (temp % 10);
+                M[l] = temp % 10;
+                temp /= 10;
+                l++;
+            }
+            
+            if(Num > INT_MAX - Mun){
+                F = 1;
+                break;
+            } else {
+                Num += Mun;
+            }
+            
+            temp = Num;
+            l = 0;
+            while(temp > 0){
+                M[l] = temp % 10;
+                temp /= 10;
+                l++;
+            }
+            
+            int isPalindrome = 1;
+            for(int i = 0; i < l / 2; i++){
+                if(M[i] != M[l - 1 - i]){
+                    isPalindrome = 0;
+                    break;
+                }
+            }
+            
+            if(isPalindrome){
+                break;
+            }
+        } while(1);
+
+        if(F == 0) printf("%d %d\n", t, Num);
+        else printf("overflow!\n");
+    }
+}
