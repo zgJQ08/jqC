1e35721176e3b412388067ea5ff36e665ac85f7b
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-13 15:32:19 +0800

    Create [ 95! ] 公平分禮物.c

---
+++ ch6 陣列/[ 95! ] 公平分禮物.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+	int main(){
+    	int gf , fd ;
+      	scanf("%d%d",&gf,&fd);
+      	int G[gf];
+      	for(int i=0 ; i<gf ; i++){
+        	scanf("%d",&G[i]);
+        }
+      int sum=0 ;
+      	for(int i=0 ; i<gf ; i++){
+        	int temp = i ;
+          	int N=0 ;
+          		if(G[i] != -1){
+                	for(int j=0 ; j<gf ; j++){
+                    	if(temp == G[j]){
+                        	N++;
+                          	G[j] = -1 ;
+                        }
+                    }
+                }
+            sum += N/fd ;
+        }
+      printf("%d",sum);
+    }
