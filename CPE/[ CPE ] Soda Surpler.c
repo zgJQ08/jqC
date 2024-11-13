13a631b83b5624bd5ec951f51a20fd7796f10cf1
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-13 22:13:21 +0800

    Update [ CPE ] Soda Surpler.c

--- CPE/[ CPE ] Soda Surpler.c
+++ CPE/[ CPE ] Soda Surpler.c
@@ -1,1 +1,16 @@
-a+b , c個空瓶=汽水 
+/*a+b , c個空瓶=汽水*/ 
+#include <stdio.h>
+    int main(){
+        int times;
+      scanf("%d",&times);
+          while(times--){
+            int a, b , n , Bo=0 ;
+            scanf("%d%d%d",&a,&b,&n);
+              a += b ;
+              while(a>=n){
+            Bo += a/n ;
+            a = a/n + a%n ;
+            }
+        printf("%d\n",Bo);
+        }
+    }
