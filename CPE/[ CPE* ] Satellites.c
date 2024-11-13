96447acbba6cb189e2524b9d22847b3158760b6f
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-13 13:18:47 +0800

    Update [ CPE* ] Satellites.c

--- CPE/[ CPE* ] Satellites.c
+++ CPE/[ CPE* ] Satellites.c
@@ -1,1 +1,18 @@
-/* 這小複雜 賭他不會考 */
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+
+#define PI 3.14159265358979323846
+
+int main(){
+    double s, a;
+    char c[5];
+    while(scanf("%lf %lf %s", &s, &a, c) != EOF){
+        if(strcmp(c, "min") == 0) a /= 60;
+        if(a > 180) a = 360 - a;
+        double ang = a * PI / 180.0;
+        double chord = 2.0 * (s + 6440.0) * sin(ang / 2.0);
+        double arc = ang * (s + 6440);
+        printf("%.6lf %.6lf\n", arc, chord);
+    }
+}
