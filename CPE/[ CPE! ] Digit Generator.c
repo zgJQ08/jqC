da1258ebdb0c0b411184f5ee3d1d151f8adff97f
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-13 13:15:46 +0800

    Update [ CPE ] Digit Generator.c

--- CPE/[ CPE! ] Digit Generator.c
+++ CPE/[ CPE! ] Digit Generator.c
@@ -1,1 +1,26 @@
+/* 找到直接break 他要比較小ㄉ 然後記得重置a!! */
+#include <stdio.h>
+	int main(){
+    	long long times ,a ;
+      	scanf("%lld",&times);
+      	while(times--){
+      	    a=0 ;
+        	long long N;
+          	scanf("%lld",&N);
+          for(long i=1 ; i<N ; i++){
+          	long long temp = i , tN=i ;
+            while(temp>0){
+            	tN += temp%10;
+              	temp /=10 ;
+            }
+            if(tN==N){
+            	a=i;
+            	break;
+            }
 
+          }
+
+        printf("%lld\n",a);
+        }
+
+    }
