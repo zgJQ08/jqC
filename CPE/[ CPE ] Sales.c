78d6461df520e855c38a18e5e1ca711816b566be
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-13 22:15:06 +0800

    Update [ CPE ] Sales.c

--- CPE/[ CPE ] Sales.c
+++ CPE/[ CPE ] Sales.c
@@ -1,1 +1,25 @@
-(20, 43, 57, 43, 20) 就是 [1,2,2,1] 後面的>=前面的就+
+/* (20, 43, 57, 43, 20) 就是 [1,2,2,1] 後面的>=前面的就+ */
+#include <stdio.h>
+    int main(){
+        int times;
+        scanf("%d",&times);
+      while(times--){
+          int nums;
+          scanf("%d",&nums);
+            int N[nums] ;
+        for(int i=0 ; i<nums ;i++){
+            scanf("%d",&N[i]);
+        }
+        int sum = 0 ;
+        for(int i=1 ; i<nums ; i++){
+            for(int j=0 ; j<i ; j++){
+            if(N[i]>=N[j]){
+                sum++ ;
+            }
+            }
+        }
+      printf("%d\n",sum);
+      }
+      
+    
+    }
