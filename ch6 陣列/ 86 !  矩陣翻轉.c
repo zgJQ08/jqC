c8f0477b104f42ea535a09eee3b5507ab11d8661
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-13 21:15:24 +0800

    Update  86 !  矩陣翻轉.c

--- ch6 陣列/ 86 !  矩陣翻轉.c
+++ ch6 陣列/ 86 !  矩陣翻轉.c
@@ -1,26 +1,26 @@
 /* 反正用一個新的倒過來裝就好 */
 #include <stdio.h>
 int main(){
-    int a , b , i=0 ,j=0 ,k=0 , l=0 , n=0 ,m=0;
+    int a , b ;
     scanf("%d%d",&a,&b) ;
-    int arr[100][100] = {0};
-    int arrw[100][100] = {0};
+    int arr[a][b] ;
+    int arrw[b][a] ;
 
-    for(i=0 ; i<a ; i++){
-        for(j=0 ; j<b ; j++){
+    for(int i=0 ; i<a ; i++){
+        for(int j=0 ; j<b ; j++){
             scanf("%d",&arr[i][j]);
         }
     }
 
-    for(int k=0 ; k<b ; k++){
-        for(int l=0 ; l<a ; l++){
-            arrw[k][l] = arr[l][k];
+    for(int i=0 ; i<b ; i++){
+        for(int j=0 ; j<a ; j++){
+            arrw[i][j] = arr[j][i];
         }
     }
 
-    for(n=0 ; n<b ; n++){
-        for(int m=0 ; m<a ; m++){
-            printf("%d ",arrw[n][m]);
+    for(int i=0 ; i<b ; i++){
+        for(int j=0 ; j<a ; j++){
+            printf("%d ",arrw[i][j]);
         }
          printf("\n");
     }
