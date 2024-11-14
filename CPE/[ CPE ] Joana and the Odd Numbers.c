8fa65e7122d8463d3f4ef97e8f520fb3da557e15
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-14 09:36:54 +0800

    Update [ CPE ] Joana and the Odd Numbers.c

--- CPE/[ CPE ] Joana and the Odd Numbers.c
+++ CPE/[ CPE ] Joana and the Odd Numbers.c
@@ -1,1 +1,18 @@
-最後一個數字就是.. 先n/2+1 然後... 2ㄉn^2 -1 最後三個加起來就好!
+/*最後一個數字就是.. 先n/2+1 然後... 2ㄉn^2 -1 最後三個加起來就好!
+*/
+#include <stdio.h>
+    int main(){
+        long long n , sum ;
+          while(scanf("%lld",&n) != EOF){
+        
+             n = n/2+1 ;
+          long long lN = 2*n*n-1 ;
+          sum = 3 * lN -6 ;
+          printf("%lld\n",sum);
+        
+        }
+    
+    
+    
+    
+    }
