2ab5c8d0755c766f4461a5fef60c35a5f73d67d9
jq <121170329+zgJQ08@users.noreply.github.com>
2024-11-23 13:07:14 +0800

    Update ? CPE | Steps.c

--- CPE/? CPE | Steps.c
+++ CPE/? CPE | Steps.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int times; 
+    scanf("%d", &times);
+    while (times--) {
+        long x, y;
+        scanf("%ld %ld", &x, &y);
+
+        long d = y - x; //總距離
+        long k = 0; //步數
+        long sum = 0;   //累積移動的距離
+
+        while (sum < d) {
+            k++;
+            sum += (k + 1) / 2; 
+        }
+
+        printf("%ld\n", k);
+    }
+    return 0;
+}
 
