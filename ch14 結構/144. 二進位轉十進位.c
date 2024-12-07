cc355b6382b9a155c846b36d31c60cefaa2e7e5f
jq <121170329+zgJQ08@users.noreply.github.com>
2024-12-07 17:15:58 +0800

    Create 144. 二進位轉十進位.c

---
+++ ch14 結構/144. 二進位轉十進位.c
@@ -0,0 +1,51 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+*/
+
+int getDecimalValue(struct ListNode* head)
+{
+   int result = 0;
+    while (head != NULL) {
+        result = result * 2 + head->val; // 每次將數字進位並加上當前位元
+        head = head->next;
+    }
+    return result;
+}
+
+
+/* 
+// 輸入二進位，並一位一位輸入，採用遞迴方式輸入，資料存放採用linked list
+// 例如數字6的二進位是 1 1 0
+// 表示有三個結構，第一個結構的val存1，第一個結構的next紀錄第二個結構，第二個結構的val存1，第二個結構的next紀錄第三個結構，第三個結構的val存0，第三個結構的next紀錄NULL
+void Construct(struct ListNode* node, int length, int num)
+{
+    int a;
+    scanf("%d", &a);
+    node->val = a;
+ 
+    if(num >= length)
+    {
+        node->next = NULL;
+        return;
+    }
+    node -> next = (struct ListNode* )malloc(sizeof(struct ListNode));
+    num++;
+    Construct(node->next, length, num);
+}
+ 
+int main()
+{
+    struct ListNode *head;
+    int n, a, length;
+    head = (struct ListNode* )malloc(sizeof(struct ListNode));
+    scanf("%d", &n);
+    while(n--)
+    {
+        scanf("%d", &length);
+        Construct(head, length, 1);
+        int ans = getDecimalValue(head);
+        printf("%d\n", ans);
+    }
+}
+*/
