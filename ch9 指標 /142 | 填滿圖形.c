2576c5656e213c81dbecdc4ff13a2ee41282e2e1
jq <121170329+zgJQ08@users.noreply.github.com>
2024-12-09 15:29:53 +0800

    Create 142 | 填滿圖形.c

---
+++ ch9 指標 /142 | 填滿圖形.c
@@ -0,0 +1,57 @@
+/*#include <stdio.h>
+#include <stdlib.h>*/
+ 
+void floodFill(int** image, int imageSize, int sr, int sc, int newColor) {
+    int oC = image[sr][sc];
+    if (oC == newColor) return; // 起始位置是目標顏色
+
+
+    void fill(int x, int y) {
+        if (x < 0 || x >= imageSize || y < 0 || y >= imageSize || image[x][y] != oC) return; //不符合換顏色規則
+
+        image[x][y] = newColor; //填顏色
+
+        //擴散
+        fill(x - 1, y);
+        fill(x + 1, y); 
+        fill(x, y - 1); 
+        fill(x, y + 1); 
+    }
+
+    fill(sr, sc); 
+}
+ 
+/* int main() {
+    int n;
+    int sr, sc, newColor;
+    scanf("%d", & n);
+    int** p = NULL;
+    p = (int**)malloc(sizeof(int*) * n);
+    for (int i = 0; i < n; i++) {
+        p[i] = (int*)malloc(sizeof(int) * n);
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int tmp;
+            scanf("%d", &tmp);
+            p[i][j] = tmp;
+        }
+    }
+    scanf("%d", &sr);
+    scanf("%d", &sc);
+    scanf("%d", &newColor);
+    floodFill(p, n, sr, sc, newColor);
+ 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", p[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < n; i++) {
+        free(p[i]);
+    }
+    free(p);
+    return 0;
+}
+*/
