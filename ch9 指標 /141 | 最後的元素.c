58f2cf6078d7035f501a9d4c39fbbb8a91b25f49
jq <121170329+zgJQ08@users.noreply.github.com>
2024-12-18 20:04:39 +0800

    Update 141 | 最後的元素.c

--- ch9 指標 /141 | 最後的元素.c
+++ ch9 指標 /141 | 最後的元素.c
@@ -1,39 +1,41 @@
 /*#include <stdio.h>
 #include <stdlib.h>
 */
-int extractMax(int* stone, int* stoneSize) {
-    if (*stoneSize == 0) return 0; 
-    int maxIndex = 0;
-    for (int i = 1; i < *stoneSize; i++) {
-        if (stone[i] > stone[maxIndex]) {
-            maxIndex = i;
-        }
-    }
-    int maxValue = stone[maxIndex]; //紀錄最大值
-
-    stone[maxIndex] = stone[*stoneSize - 1];
-    (*stoneSize)--;
-
-    return maxValue;
-}
-
-// 插入值在最後面
-
-void insert(int* stone, int* stoneSize, int value) {
-    stone[*stoneSize] = value;
-    (*stoneSize)++;
-}
-
 int lastStoneWeight(int* stones, int stonesSize) {
-    while (stonesSize > 1) {
-        int y = extractMax(stones, &stonesSize);
-        int x = extractMax(stones, &stonesSize);
+    int y, x, Ly, Lx;
 
-        if (x != y) {
-            insert(stones, &stonesSize, y - x); 
+    while (1) {
+        int NN = 0, ON ;
+        for (int i = 0; i < stonesSize; i++) {
+            if (stones[i] == -1) {
+                NN++;
+            } else {
+                ON = stones[i];
+            }
+        }
+        if (NN == stonesSize - 1) return ON;  
+        else if (NN == stonesSize) return 0;  
+        int y = -1 , x = -1;
+        for (int i = 0; i < stonesSize; i++) {
+            if (stones[i] > y && stones[i] != -1) { // 修正：排除 stones[i] == -1
+                x = y;     // 更新次大值
+                Lx = Ly; //更新次大值
+                y = stones[i];
+                Ly = i;
+            } else if (stones[i] > x && stones[i] != -1) { // 排除 stones[i] == -1
+                x = stones[i];
+                Lx = i;
+            }
+        }
+
+        if (x == y) {
+            stones[Ly] = -1;
+            stones[Lx] = -1;
+        } else {
+            stones[Lx] = -1;
+            stones[Ly] = y - x;
         }
     }
-    return stonesSize == 1 ? stones[0] : 0; 
 }
 /*
 int main() {
@@ -41,12 +43,12 @@ int main() {
     int ans;
     scanf("%d", &n);
     int* a = (int*)malloc(n * sizeof(int));
- 
+
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
     ans = lastStoneWeight(a, n);
-    printf("%d", ans);
+    printf("%d\n", ans);
     free(a);
     return 0;
 }
