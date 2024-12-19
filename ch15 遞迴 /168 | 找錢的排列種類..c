6807492b0af8ef70b8f3558c942fdacbe43047bd
jq <121170329+zgJQ08@users.noreply.github.com>
2024-12-19 15:03:01 +0800

    Update 168 | 找錢的排列種類..c

--- ch15 遞迴 /168 | 找錢的排列種類..c
+++ ch15 遞迴 /168 | 找錢的排列種類..c
@@ -1,25 +1,18 @@
-#include <stdio.h>
+//k=0 只有一種排列方式 、 k<0 無效 
 
-int moneyPerm(int K) {
-
-    if (K == 0) { //完成了一種排序
-        return 1;
-    }
-
-    if (K < 0) { //無效排序
-        return 0;
-    }
-
-    
-    return moneyPerm(K - 1) + moneyPerm(K - 5) + moneyPerm(K - 10); //選擇不同種類硬幣
+//#include <stdio.h>
+ 
+int moneyPerm(int K){
+  if(K==0) return 1 ;
+  if(K<0) return 0;
+  return moneyPerm(K-1) + moneyPerm(K-5) + moneyPerm(K-10);
 }
 
-
-
- 
+/*
 int main(){
   int money;
   scanf("%d", &money);
   printf("%d", moneyPerm(money));
   return 0;
 }
+*/
