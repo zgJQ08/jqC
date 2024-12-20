94d3ce6793d2c5cdb48a045ce9487fb67747088a
jq <121170329+zgJQ08@users.noreply.github.com>
2024-12-20 14:05:47 +0800

    Update 145 $ 奇前偶後.c

--- ch14 結構/*145 | 奇前偶後.c
+++ ch14 結構/*145 | 奇前偶後.c
@@ -1,43 +1,39 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-
-
+/*
 struct ListNode
 {
     int val;
     struct ListNode *next;
 };
 */
-
 struct ListNode* oddEvenList(struct ListNode* head)
 {
-    if (head == NULL || head->next == NULL) {
-        return head; // 空或單節點直接返回
+  	//head=[1] [2] [3] [4] [5] NULL 
+    if(head == NULL || head->next == NULL) return head ; //奇葩情況
+  	struct ListNode *odd = head ; //odd指針 指向[1]
+  	struct ListNode *even = head->next ; //even指針 指向[2]
+  	struct ListNode *evenHead = even; //保存even指針 [2]的位置
+  	while(even !=NULL && even->next !=NULL){ //確保even 跟 下一個odd是有東西的
+    //odd連結odd 然後下一個
+    odd -> next = even -> next ; 
+    odd = odd -> next ; 
+    //even連結even 然後下一個
+    even -> next = odd -> next ;
+    even = even -> next;   
     }
-
-    struct ListNode *odd = head;          // 奇數位置
-    struct ListNode *even = head->next;   // 偶數位置
-    struct ListNode *evenHead = even;     // 儲存偶數的起點
-
-    while (even != NULL && even->next != NULL) {
-        odd->next = even->next;           // 奇數鏈接下一個奇數
-        odd = odd->next;
-        even->next = odd->next;           // 偶數鏈接下一個偶數
-        even = even->next;
-    }
-
-    odd->next = evenHead;                 // 奇數鏈接偶數鏈表
-    return head;
+  odd->next = evenHead ; //odd串接even
+  return head ;
 }
-
 /*
 void Construct(struct ListNode* node, int length, int num)
 {
     int a;
     scanf("%d", &a);
     node->val = a;
-
-    if (num >= length) {
+ 
+    if(num >= length)
+    {
         node->next = NULL;
         return;
     }
@@ -45,28 +41,28 @@ void Construct(struct ListNode* node, int length, int num)
     num++;
     Construct(node->next, length, num);
 }
-
+ 
 int main()
 {
     struct ListNode* head;
-    int n, length;
+    int n, a, length;
     head = (struct ListNode* )malloc(sizeof(struct ListNode));
-    scanf("%d", &n); // 輸入有幾組數列
-    while (n--) {
-        scanf("%d", &length); // 輸入數列長度
+    scanf("%d", &n);
+    while(n--)
+    {
+        scanf("%d", &length);
         Construct(head, length, 1);
         struct ListNode* ans = oddEvenList(head);
         struct ListNode* ptr;
-
+ 
         ptr = ans;
-
-        // 輸出結果
-        while (ptr != NULL) {
+ 
+        while(ptr != NULL)
+        {
             printf("%d ", ptr->val);
             ptr = ptr->next;
         }
         printf("\n");
     }
-    return 0;
 }
 */
