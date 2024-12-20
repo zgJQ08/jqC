b2d49a0411ee11593491c0e09babcdc4eb2c7e6a
jq <121170329+zgJQ08@users.noreply.github.com>
2024-12-21 00:36:41 +0800

    Update -53 ! Permutation.c

--- ch14 結構/-53 ! Permutation.c
+++ ch14 結構/-53 ! Permutation.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-void ptPer(int N, int result[]) { //印出來
-    for (int i = 0; i < N; i++) {
-        if (i == 0) printf("%d", result[i]);
-        else printf(" %d", result[i]);
-    }
-    printf("\n");
-}
 
 // 全排列
 // idx // N 有幾個要排列 // num[]提供排列的原始數列 //used[]用來標記num[]有沒有被使用過 //result[]存放排列結果
 void permute(int idx, int N, int num[], int used[], int result[]) {
     // 如果已選擇了 N 個數字，印出
     if (idx == N) {
-        ptPer(N, result);
+        void ptPer(int N, int result[]) { //印出來
+    for (int i = 0; i < N; i++) {
+        if (i == 0) printf("%d", result[i]);
+        else printf(" %d", result[i]);
+    }
+    printf("\n");
+}
         return;
     }
 
