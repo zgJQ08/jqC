854792afe754af50dd2b8ca3d8a65f55b97628b0
jq <121170329+zgJQ08@users.noreply.github.com>
2024-12-25 21:28:28 +0800

    Update 124' The Hotel with Infinite Rooms.c

--- ch8 函式/124' The Hotel with Infinite Rooms.c
+++ ch8 函式/124' The Hotel with Infinite Rooms.c
@@ -1,15 +1,15 @@
 //#include <stdio.h>
+
 int D_size(int S, int D) {
-    int day = 1;   
-    while (1) {    
-        if (day <= D && D < day + S) {   //檢查天數是否在入住範圍內
-            return S;  
-        }
-        
-        day += S;  //幾天後換下一組
-        S++; //之後人數多一 
+    int day = 1; //第一天
+    while(1){
+        if( day <= D && D < day+S) return S; //測試有無在範圍裡面
+        day += S; //下一輪
+        S++; //每輪多一
     }
 }
+
+ 
 /*
 int main(){
     int S, D;
