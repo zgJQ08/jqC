759a5149ee21435b519e1790cf72a1725c41be5b
jq <121170329+zgJQ08@users.noreply.github.com>
2024-12-25 12:59:02 +0800

    Create 143 $ 學號與分數.c

---
+++ ch14 結構/143 $ 學號與分數.c
@@ -0,0 +1,16 @@
+/*#include<stdio.h>
+ 
+struct Student{
+    int m_id;
+    int m_score;
+};
+ 
+int main(){
+    struct Student x;
+  */
+    scanf("%d%d",&x.m_id,&x.m_score);
+  /*
+    printf("(ID: %d, score: %d)", x.m_id, x.m_score);
+    return 0;
+}
+*/
